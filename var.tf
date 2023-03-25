04197e11c96d128a8eab9f6fb85fb8ba02bce7b5
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-03-24 20:35:09 -0400

    Terraform: create storage bucket in GCP

---
+++ var.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  description = "The ID of the GCP project."
+  default     = "avian-amulet-378416"
+}
+
+variable "bucket_name" {
+  description = "A list of unique names for the GCP storage buckets."
+  type        = list(string)
+  default     = [
+    "my-test-bucket-latest8999",
+    "mytestbucket8888",
+    "montrealcollege-927218-for-demo",
+    "bucket-full-of-happiness"
+
+  ]
+}
+variable "bucket_location" {
+  description = "The location of the GCP storage buckets."
+  default     = "US"
+}
